9ee69407a344a4a94514b82a5ab5056d5d17d0a0
SgtHero <futuress@gmx.net>
2016-04-19 10:20:07 +0200

    template update

--- client/helper/RealStateHelper.js
+++ client/helper/RealStateHelper.js
@@ -1,5 +1,5 @@
 Template.RealState.helpers({
-    morningFunction : function () {
+    morningFunction: function () {
         var query = MedicationPlan.findOne({_id: this._id});
         var morningQuanitity = query.morning;
         if (morningQuanitity < 3 && morningQuanitity > 0) {
@@ -8,7 +8,7 @@ Template.RealState.helpers({
             return 'fa fa-ban status-grey'
         }
     },
-    middayFunction : function () {
+    middayFunction: function () {
         var query = MedicationPlan.findOne({_id: this._id});
         var middayQuanitity = query.midday;
         if (middayQuanitity < 3 && middayQuanitity > 0) {
@@ -17,7 +17,7 @@ Template.RealState.helpers({
             return 'fa fa-ban status-grey'
         }
     },
-    eveningFunction : function () {
+    eveningFunction: function () {
         var query = MedicationPlan.findOne({_id: this._id});
         var eveningQuanitity = query.evening;
         if (eveningQuanitity < 3 && eveningQuanitity > 0) {
@@ -26,7 +26,7 @@ Template.RealState.helpers({
             return 'fa fa-ban status-grey'
         }
     },
-    nightFunction : function () {
+    nightFunction: function () {
         var query = MedicationPlan.findOne({_id: this._id});
         var morningQuanitity = query.night;
         if (morningQuanitity < 3 && morningQuanitity > 0) {
@@ -34,14 +34,6 @@ Template.RealState.helpers({
         } else {
             return 'fa fa-ban status-grey'
         }
-    },
-    timerFunction : function () {
-        function f() {
-            var status = "status-yellow";
-            console.log(status);
-            return status
-        }
-        setTimeout(f, 2000);
     }
 });
 
