896c5d5ed07dcc33e6789369f165022ffff3203a
SgtHero <futuress@gmx.net>
2016-04-19 14:58:40 +0200

    test fehler behoben und yml umgeändert

--- tests/gagarin/tests.js
+++ tests/gagarin/tests.js
@@ -1,11 +1,11 @@
 /**
  * Created by christianschulz1 on 19.04.16.
  */
-describe('Mein erster Gagarin-Test', function () {
-   var server = meteor();
+describe('My first Gagarin test suite', function () {
+    var server = meteor();
     it('should just work', function () {
         return server.execute(function () {
-            console.log("Ich lebe!!!");
+            console.log('I am alive!');
         });
     });
 });
